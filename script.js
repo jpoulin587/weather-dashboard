09a0c0638028449be5e78a8f81a9142f271cdea1
Jim Poulin <jpoulin587a@gmail.com>
2021-06-25 14:59:58 -0500

    trying to add second api call with LON/LAT

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 // sample call  https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0b17ac4effa78f1520a5cf1ab7d339e5
 //https://api.openweathermap.org/data/2.5/onecall
 
+let currentCityLon;
+let currentCityLat;
+
+
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&APPID=0b17ac4effa78f1520a5cf1ab7d339e5', {
     // The browser fetches the resource from the remote server without first looking in the cache.
     // The browser will then update the cache with the downloaded resource.
@@ -12,6 +17,33 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&
       return response.json();
     })
     .then(function (data) {
+     currentCityLon = data.coord.lon;
+     currentCityLat = data.coord.lat;  
+      /* console.log(data);
+      console.log('Weather data \n----------');
+      console.log(data.name);
+      console.log(data.main.humidity);
+      console.log(data.main.temp);
+      console.log(data.wind.speed);
+      console.log(data.weather[0].description); */
+      console.log(currentCityLon);
+      console.log(currentCityLat);
+});
+
+console.log(currentCityLon);
+console.log(currentCityLat);
+
+fetch('https://api.openweathermap.org/data/2.5/onecall?Lat=' + currentCityLat +'&Lon=' + currentCityLon + '&units=imperial&APPID=0b17ac4effa78f1520a5cf1ab7d339e5', {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    cache: 'reload',
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+     
       console.log(data);
       console.log('Weather data \n----------');
       console.log(data.name);
@@ -19,5 +51,5 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&
       console.log(data.main.temp);
       console.log(data.wind.speed);
       console.log(data.weather[0].description);
-      console.log(data.temp);
+
 });
