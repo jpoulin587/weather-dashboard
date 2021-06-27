ce9fe81fc5b32ad27cf19c7d2c830127e943096a
Jim Poulin <jpoulin587a@gmail.com>
2021-06-27 15:05:15 -0500

    extract data for the 5day forecast

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let currentCityLat;
 let currentCityPull;
 let currentDate;
 let myDate;
-let forecastDate1;
-let forecastDate2;
-let forecastDate3;
-let forecastDate4;
-let forecastDate5;
+//let forecastDate1;
+//let forecastDate2;
+//let forecastDate3;
+//let forecastDate4;
+//let forecastDate5;
 let cityName = $('#city-name');
 let todayDate = $('#today-date');
 let currentTemp = $('#current-temp');
@@ -19,6 +19,9 @@ let currentWind = $('#current-wind');
 let currentHumidity = $('#current-humidity');
 let currentUiv = $('#current-uvi');
 let currentDesc = $('#current-description');
+let forecastDay;
+let fiveDayList = $('five-day-list');
+let fiveDayData = []
 
 
 function getWeather() {
@@ -61,17 +64,11 @@ function getWeather() {
 
 function populateCurrent(data) {
   console.log(data);
-  console.log("Weather data from function \n----------");
-  console.log("current city " + currentCityPull)
-  console.log("current date " + data.current.dt);
-  console.log("current humid " + data.current.humidity);
-  console.log("current temp " + data.current.temp);
-  console.log("current wind " + data.current.wind_speed);
-  console.log("current UV Index " + data.current.uvi);
-  console.log("current description " + data.current.weather[0].description);
-  let nowUvi = data.current.uvi * 10;
 
+  // show current data  on the index.html
+  let nowUvi = data.current.uvi * 1;
   cityName.text("Current City: " + currentCityPull);
+  //TODO this date is not pulling. 
   todayDate.text("Today's Date: " + myDate);
   currentDesc.text("Current Conditions: " + data.current.weather[0].description);
   currentTemp.text("Current Temp: " + data.current.temp + "F");
@@ -79,6 +76,7 @@ function populateCurrent(data) {
   currentHumidity.text("Current Humidity: " + data.current.humidity + "%");
   currentUiv.text("Current UV Index: " + nowUvi)
 
+  // This makes the color of the UV Index adjust with the severity 
   //TODO How do I add also make the text white with the green and red background
   if (nowUvi <= 3.0) {
     $("#current-uvi").css("background-color", "green");
@@ -88,14 +86,67 @@ function populateCurrent(data) {
     $("#current-uvi").css("background-color", "red");
   };
 
+  // populate the 5-day forecast
+  console.log("Weather data for day one \n----------");
+  console.log("Forecast date " + data.daily[0].dt);
+  console.log("Forecast description " + data.daily[0].weather[0].description);
+  console.log("Forecast humid " + data.daily[0].humidity);
+  console.log("Forecast hi temp " + data.daily[0].temp.max);
+  console.log("Forecast lo temp " + data.daily[0].temp.min);
+  console.log("Forecast wind " + data.daily[0].wind_speed);
+  //console.log(data.daily);
 
-}
-//converts UNIX date to a human date
+
+  // make an array of the date we need
+  fiveDayData.push(data.daily[0].dt, data.daily[0].weather[0].description, data.daily[0].humidity, data.daily[0].temp.max, data.daily[0].temp.min, data.daily[0].wind_speed);
+  console.log(fiveDayData);
+
+  //TODO go back another time a refactor this to make it work
+  // TODO this should populate the 5-day forecast blocks
+
+  let forecastDay = data.daily;
+  console.log(forecastDay);
+  for (let i = 0; i < 5; i++) {
+    //const dailyData = forecastDay[i];
+    //console.log(dailyData);
+    console.log("date day " + i + " " + data.daily[i].dt);
+    console.log("outlook day " + i + " " + data.daily[i].weather[0].description);
+    console.log("humid day " + i + " " + data.daily[i].humidity);
+    console.log("hi day " + i + " " + data.daily[i].temp.max);
+    console.log("low day " + i + " " + data.daily[i].temp.min);
+    console.log("wind day " + i + " " + data.daily[i].wind_speed);
+
+  }
+
+  //   currentQuestion.choices.forEach(function(choice, i){
+  //console.log(questionIndex + "questionIndex")
+  //console.log(i);
+  //console.log(choice);
+  //     let choiceButton = document.createElement("button");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+//converts current UNIX date to a human date
 function convertDate(data) {
   currentDate = data.current.dt
   // from https://www.epochconverter.com/programming/#javascript
   var myDate = new Date(currentDate * 1000);
-  console.log("function result" + myDate);
+  console.log("Current human date: " + myDate);
+  return myDate;
 };
 
 
@@ -106,7 +157,7 @@ function convertDate(data) {
 getWeather();
 
 /*
-//---Search field and history list
+//---TODO Search field history list
 let citySearch = $('#city-search');
 let searchHistory = $('#search-history');
 let searchInput;
@@ -131,6 +182,7 @@ function handleFormSubmit(event) {
 
 citySearch.on('submit', handleFormSubmit);
 */
+
 
 
 
