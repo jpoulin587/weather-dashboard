67741db41dfa6298da144d242e77e49f7082eb84
Jim Poulin <jpoulin587a@gmail.com>
2021-06-29 12:32:51 -0500

    added search histoy list.  Not a link yet

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ let currentUiv = $('#current-uvi');
 let currentDesc = $('#current-description');
 let forecastDay;
 let fiveDayList = document.getElementById("five-day-list");
+let citySearch = $('#city-search');
+let searchHistory = $('#search-history');
 let searchInput = "bar harbor";
 
 
@@ -161,6 +163,16 @@ function SearchCityForm(e) {
   searchInput = document.getElementById("search-input").value;
   console.log('search', searchInput)
   e.preventDefault();
+  searchInput = $('input[name="search-input"]').val();
+  if (!searchInput) {
+    console.log('No city entered into form!');
+    return;
+  }
+  searchHistory.append('<li>' + searchInput + '</li>');
+
+  $('input[name="search-input"]').val('');
+
+
   getWeather();
 };
 
